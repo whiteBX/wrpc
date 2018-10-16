f80f5867707b27637cf7bf57714034148b4e2672
xiong.bai <xiong.bai@qunar.com>
2018-10-16 19:41:48 +0800

    RPC第二版:bean注册与客户端通过接口调用实现

--- src/main/java/org/white/wrpc/provider/handler/RpcServerNettyHandler.java
+++ src/main/java/org/white/wrpc/provider/handler/RpcServerNettyHandler.java
@@ -22,12 +22,19 @@ public class RpcServerNettyHandler extends ChannelInboundHandlerAdapter {
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         System.out.println("服务端收到请求:" + msg);
         try {
+            // 解析出 类名+方法名+请求参数类型(方法签名)
             String[] splitParam = msg.toString().split(ProviderConstant.DOLLAR_SPLIT);
             String[] beanMessage = splitParam[0].split(ProviderConstant.SHARP_SPLIT);
+            // 获取注册的服务
             Object object = ProviderBeanHolder.getBean(beanMessage[0]);
+            if (object == null) {
+                System.out.println("服务类未注册:" + beanMessage[0]);
+            }
+            // 通过反射调用服务
             Class paramType = Class.forName(beanMessage[2]);
             Method method = object.getClass().getDeclaredMethod(beanMessage[1], paramType);
             Object response = method.invoke(object, JSON.parseObject(splitParam[1], paramType));
+            // 请求响应
             ctx.writeAndFlush(JSON.toJSONString(response));
         } catch (Exception e) {
             System.out.println("服务异常");
